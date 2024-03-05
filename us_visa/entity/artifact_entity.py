d700ef75863c8b9081cf50760e3c22888f03b5a6
hariravikumar <harik76@gmail.com>
2024-03-05 17:06:41 -0500

    Model Trainer

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -17,3 +17,17 @@ class DataTransformationArtifact:
     transformed_object_file_path:str 
     transformed_train_file_path:str
     transformed_test_file_path:str    
+
+
+@dataclass
+class ClassificationMetricArtifact:
+    accuracy:float
+    f1_score:float
+    precision_score:float
+    recall_score:float
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str         # for Model path
+    metric_artifact:ClassificationMetricArtifact    #for Classification Metric f1 score,precision,recall
